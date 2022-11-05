615c33e6ca7ffd711ca87e743a0827d0e4b74955
Matthew <chomatt@HIVE>
2022-11-05 15:58:03 +0800

    added gesture display & refactored javascript

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const allBtns = document.querySelectorAll('.btn')
-const display = document.querySelector('.display')
+const display = document.querySelector('.text-display')
 const images = document.querySelectorAll('.gesture-img')
+const gestureDisplay = document.querySelectorAll('.gesture-display')
 const playerScore = 0;
 const computerScore = 0;
 
-// * Win, Lose, Tie logic
-function win(){
-    display.innerHTML = 'You Win'
+function computer(){
+    const gestureArr = ['Scissor', 'Rock', 'Paper'];
+    const randNum = Math.floor(Math.random() * gestureArr.length);
+    const cGesture = gestureArr[randNum];
+    return cGesture
 }
 
-function lose(){
-    display.innerHTML = 'You Lose'
+function player(e){
+    const pGesture = e.target.id
+    return pGesture
 }
 
-function tie(){
-    display.innerHTML = 'Tie'
+function imageDisplay(computer, player){
+    const cGestureDisplay = computer
+    const pGestureDisplay = player
+    const cDisplay = gestureDisplay[1].setAttribute('src', `gestures/${cGestureDisplay}.png`)
+    const pDisplay = gestureDisplay[0].setAttribute('src', `gestures/${pGestureDisplay}.png`)
 }
+
 
 function gameMechanics(){
-  
     allBtns.forEach((btn) => {
-        
         btn.addEventListener('click', (e) => {
             // * 3D Button animation
             const btnClass = btn.classList
             btnClass.add('btn-clicked');
             setTimeout(() => btnClass.remove('btn-clicked'), 100)
 
-            
-            // *  Computer Gesture Logic: 
-            const computer = ['Scissor', 'Rock', 'Paper'];
-            const randNum = Math.floor(Math.random() * computer.length);
-            const computerGesture = computer[randNum];
-
-            // * Player Gesture & win, lose declaration
-            const playerGesture = e.target.id
+            const computerGesture = computer()
+            const playerGesture = player(e)
+            imageDisplay(computerGesture, playerGesture)
             if (playerGesture == computerGesture){
                 tie()
             } else if (playerGesture == 'Rock' && computerGesture == 'Scissor'){
@@ -50,12 +51,24 @@ function gameMechanics(){
             } else if (playerGesture == 'Scissor' && computerGesture == 'Rock'){
                 lose()
             };
-
-            
         })
     })
     
 
+}
+
+
+// * Win, Lose, Tie logic
+function win(){
+    display.innerHTML = 'You Win'
+}
+
+function lose(){
+    display.innerHTML = 'You Lose'
+}
+
+function tie(){
+    display.innerHTML = 'Tie'
 }
 
 gameMechanics()
